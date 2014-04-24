89e07f21bfa19517fa08b25f6899d68243f8af4f
Starli0n <Starli0n@users.noreply.github.com>
2014-04-24 10:29:54 +0200

    Message error handling

--- slack.py
+++ slack.py
@@ -1,4 +1,6 @@
 import sublime, sublime_plugin
+import functools
+import json
 exec_mod = __import__("exec")
 
 plugin_name = 'Slack'
@@ -10,9 +12,10 @@ class SlackCommand(exec_mod.ExecCommand):
 		s = sublime.load_settings(plugin_name + '.sublime-settings')
 		self.token = s.get('token', None)
 		self.http_proxy = s.get('http_proxy', None)
+		self.show_panel_on_build = s.get('show_panel_on_build', False)
+		self.show_panel_on_build_pref = sublime.load_settings("Preferences.sublime-settings").get("show_panel_on_build", True)
 
 		self.http_request('auth.test')
-		sublime.status_message("slack done")
 
 	def http_request(self, method):
 		url = base_url.format(method=method, token=self.token)
@@ -21,9 +24,27 @@ class SlackCommand(exec_mod.ExecCommand):
 		if self.http_proxy is not None:
 			command.append('-x')
 			command.append(self.http_proxy)
-		#sublime.load_settings("Preferences.sublime-settings").set("show_panel_on_build", True)
-		super(SlackCommand, self).run(cmd = command)
+		sublime.load_settings("Preferences.sublime-settings").set("show_panel_on_build", self.show_panel_on_build)
+		try:
+			super(SlackCommand, self).run(cmd = command)
+		finally:
+			sublime.load_settings("Preferences.sublime-settings").set("show_panel_on_build", self.show_panel_on_build_pref)
+
+	def http_result(self, proc, data):
+		result = json.loads(data)
+		self.isOk = result["ok"]
+		self.error = result.get('error')
+
+	def http_finish(self, proc):
+		if self.isOk:
+			sublime.status_message("Slack request success")
+		else:
+			sublime.status_message("Slack request error: " + self.error)
 
 	def on_data(self, proc, data):
 		super(SlackCommand, self).on_data(proc, data)
-		print data
+		sublime.set_timeout(functools.partial(self.http_result, proc, data), 0)
+
+	def on_finished(self, proc):
+		super(SlackCommand, self).on_finished(proc)
+		sublime.set_timeout(functools.partial(self.http_finish, proc), 0)
